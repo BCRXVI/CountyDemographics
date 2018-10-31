2dde85b9af9dd24a3677129d4124435e2dd5135a
BCRXVI <42652884+BCRXVI@users.noreply.github.com>
2018-10-31 11:33:19 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,21 +2,5 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/") #annotation tells the URL that will amek this function run
-def render_main():
-    return render_template('home.html')
-
-@app.route("/page1")
-def render_page1():
-    return render_template('page1.html')
-
-@app.route("/page2")
-def render_page2():
-    return render_template('page2.html')
-
-@app.route("/page3")
-def render_page3():
-    return render_template('page3.html')
-  
   if __name__=="__main__":
     app.run(debug=False, port=54321)
