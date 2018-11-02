f242b68bc44f2a05da6bbbaab9c5c436851d9307
BCRXVI <blake.c.robinson.xvi@gmail.com>
2018-11-02 11:12:31 -0700

    'jkasdfjklh

--- webapp.py
+++ webapp.py
@@ -1,14 +1,18 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, Markup
 import os
 import json
 app = Flask(__name__)
 
 @app.route("/") #annotation tells the URL that will amek this function run
 def render_main():
-    return render_template('home.html')
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    return render_template('home.html', states = get_state_options(counties))
   
 def get_state_options(counties):
   bom=[]
+  
+
   
   for county in counties:
     state = county["State"]
@@ -19,9 +23,13 @@ def get_state_options(counties):
       
   options = ""
   for state in bom:
-    options += Markup("<option value=\"" + state + "\">" + c + "</option>")
-  
-  print(options)
+    options += Markup("<option value=\"" + state + "\">" + state + "</option>")
 
-  if __name__=="__main__":
-    app.run(debug=True, port=42069)
+  return options
+  
+  def fun_fact(state):
+    
+    for county in 
+    
+if __name__=="__main__":
+    app.run(debug=True, port=66666)
