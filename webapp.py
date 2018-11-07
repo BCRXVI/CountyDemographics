bb7c1192b7a1ccf12eb371bd6c36840c7d83a7d2
BCRXVI <blake.c.robinson.xvi@gmail.com>
2018-11-07 12:43:00 -0800

    wfdg

--- webapp.py
+++ webapp.py
@@ -3,11 +3,30 @@ import os
 import json
 app = Flask(__name__)
 
-@app.route("/") #annotation tells the URL that will amek this function run
+def fun_fact(state):
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    boi = 0
+    highestcounty = ''
+    for county in counties:
+        if county["State"] == state:
+            if (county['Income']['Median Household Income'] > boi):
+                boi = county['Income']['Median Household Income']
+                highestcounty = county['County']
+    return state + ' ' + highestcounty + ' ' + str(boi)
+
+@app.route("/") #annotation tells the URL that will make this function run
 def render_main():
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
-    return render_template('home.html', states = get_state_options(counties))
+    try:
+        state = request.args['states']
+        print(state)
+        ff=fun_fact(state)
+        print(ff)
+        return render_template('home.html', states = get_state_options(counties), funfact = ff)
+    except:
+        return render_template('home.html', states = get_state_options(counties))
   
 def get_state_options(counties):
   bom=[]
@@ -26,10 +45,8 @@ def get_state_options(counties):
     options += Markup("<option value=\"" + state + "\">" + state + "</option>")
 
   return options
-  
-  def fun_fact(state):
-    
-    for county in 
+ 
+
     
 if __name__=="__main__":
     app.run(debug=True, port=66666)
